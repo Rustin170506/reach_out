b726cf785ce28a95eded50d6ddf22cb89868cc4d
hi-rustin <rustin.liu@gmail.com>
2021-08-23 22:10:43 +0800

    Send email with sendgrid template

--- lib/reach_out/reach_out_email.ex
+++ lib/reach_out/reach_out_email.ex
@@ -3,9 +3,9 @@ defmodule ReachOut.ReachOutEmail do
 
   def reach_out(%{name: name, email: email}) do
     new()
+    |> put_provider_option(:template_id, "d-e82ee6e445804b9faab5fd5efd00e38c")
+    |> put_provider_option(:dynamic_template_data, %{name: name})
     |> to({name, email})
     |> from({"Rustin Liu", "rustin.liu@gmail.com"})
-    |> subject("Hello, #{name}")
-    |> text_body("Hello #{name}\n")
   end
 end
